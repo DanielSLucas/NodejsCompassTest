0e6aae2bb36b7d4f4b3a8bbdd04fd668ad59299e
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2022-04-05 13:41:43 -0300

    initial config

---
+++ src/shared/http/server.ts
@@ -0,0 +1,8 @@
+import { app } from "./app";
+
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server listening on port ${port}`);
+});
